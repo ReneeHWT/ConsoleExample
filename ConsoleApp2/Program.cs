8aaaf8a849099ff7c194d1100b644eda52f1be45
ReneeHWT <rhuang83@gmail.com>
2020-11-12 19:23:17 +0800

    first draft

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 433, 879, 234, 423, 542 };
+            Console.WriteLine(arr[0] + "," + arr[1] + "," + arr[2]);
+            Console.WriteLine(arr.Length);
+
+            /*string name = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                case 2:
+                case 100:
+                    Console.WriteLine("You are VIP.");
+                    break;
+                case 50:
+                    Console.WriteLine("You are normal.");
+                    break;
+                default:
+                    Console.WriteLine("You are`t anything.");
+                    break;
+            }
+
+            if (name == "Renee")
+            {
+                Console.WriteLine("You are VIP.");
+            }
+            else if (name == "Ch")
+            {
+                Console.WriteLine("You are normal.");
+            }
+            else
+            {
+                Console.WriteLine("You are`t anything.");
+            }*/
+        }
+    }
+}
